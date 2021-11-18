845350725c7df787a7cbd9c3061ffc1f406d014a
Sutirtha Nath <87424205+Sutirtha2000@users.noreply.github.com>
2021-11-18 23:14:09 +0530

    source.js

---
+++ source.js
@@ -0,0 +1,15 @@
+let countEl=document.getElementById("counter")
+let saveEl=document.getElementById("save")
+let entriesEl=document.getElementById("entries")
+let count=0
+function incrementFun()
+{
+    count+=1;
+    countEl.textContent=count;
+}
+saveEl.addEventListener("click",function(){
+    let countStr=count+" - "
+    entriesEl.textContent+=countStr;
+    count=0
+    countEl.textContent=count
+})
